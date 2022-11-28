172e06435ee590fb0872250f86d97ab9965b35dd
min <earnestga@naver.com>
2022-11-28 11:47:55 +0900

    web project v1

---
+++ lib/dbconn.php
@@ -0,0 +1,15 @@
+<?
+    $connect=mysql_connect( "localhost", "earnestga3", "alsrud92!!") or  
+        die( "SQL server에 연결할 수 없습니다."); 
+
+    mysql_select_db("earnestga3",$connect);
+    //     $connect=mysql_connect( "localhost", "mk1", "1234") or  
+    //     die( "SQL server에 연결할 수 없습니다."); 
+
+    // mysql_select_db("mk1_db",$connect);
+
+    //     $connect=mysql_connect( "localhost", "mk", "1234") or  
+    //     die( "SQL server에 연결할 수 없습니다."); 
+
+    // mysql_select_db("mk_db",$connect);
+?>
